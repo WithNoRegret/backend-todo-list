e58bfb4d3881e81db92067a71dfb82419432d1e6
Sergey Pjankov <no.regret.ccg@gmail.com>
2024-07-28 22:19:47 +0300

    added cors

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 import taskRouter from './routes/tasks.js';
 
 const app = express();
 // Middlewares
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/api/tasks', taskRouter);
 
 export default app;
